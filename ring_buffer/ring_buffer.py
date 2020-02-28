e188df71103728e7b35393afe105d09d78338ae1
Tyler Berrett <btylerberrett@gmail.com>
2020-02-28 09:50:46 -0700

    add functionality to ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,16 +8,40 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current:
+            if self.current == self.capacity:
+                self.current = None
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        else:
+            if self.current is None:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = 1
+            else:
+                location = self.storage.head
+                i = 0
+                while location:
+                    if location.value:
+                        if i == self.current:
+                            location.value = item
+                            self.current += 1
+                            break
+                        location = location.next
+                    i += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
-        
+        start = self.storage.head
+        while start:
+            if start.value:
+                list_buffer_contents.append(start.value)
+                start = start.next
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
